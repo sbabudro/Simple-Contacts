9877f3df0306cea8b20b3ea2202f727eeef8f8ff
Skyler Babudro <sbabudro@gmail.com>
2020-08-28 17:41:43 -0300

    working on authentication

--- donsdirlib/src/main/java/com/donsdirectory/donsdirlib/activities/LoginActivity.kt
+++ donsdirlib/src/main/java/com/donsdirectory/donsdirlib/activities/LoginActivity.kt
@@ -2,12 +2,16 @@ package com.donsdirectory.donsdirlib.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import android.text.Editable
+import android.util.Base64.DEFAULT
+import android.util.Base64.encode
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -15,6 +19,7 @@ import com.donsdirectory.donsdirlib.R
 //import androidx.lifecycle.MutableLiveData
 import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONException
+import java.util.*
 import kotlin.properties.Delegates
 
 const val SUCCESS = true
@@ -36,13 +41,14 @@ class LoginActivity : AppCompatActivity() {
         LoginButton.setOnClickListener {
             Log.d("ShowMeOnly", "clicked")
             val loginResult = authenticateLogin(username.text, password.text)
-            if(loginResult == 2) {
+            Log.d("ShowMeOnly",loginResult.toString())
+            if (loginResult == 1) {
                 makeToast("Log-in Successful!")
-//                finish()
+                finish()
                 //TODO: change main activity using logged-in state (maybe pass a success/fail variable?)
-            } else if(loginResult == 0) {
+            } else if (loginResult == 0) {
                 makeToast("Invalid username or password")
-            } else if(loginResult == -1) {
+            } else if (loginResult == -1) {
                 makeToast("Connection failed!")
             }
         }
@@ -58,42 +64,89 @@ class LoginActivity : AppCompatActivity() {
 
     //This function receives the username and password entered and performs a server call to check
     //if the login was successful before bringing the user to the main screen
+
+    private fun checkPassword(token: String, password: String, requestQueue: RequestQueue): Int? {
+        val url = "https://dev.donsdirectory.com/api_auth.php?t=$token&p=$password"
+        var authenticated: Int? = null
+        Log.d("URL", url)
+
+
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                { response ->
+                    if (response == null) {
+//                    textView.text = "Failed"
+                    } else{
+                        authenticated = response.toInt()
+                        Log.d("Authentication","$authenticated")
+                    }
+                },
+                { error -> Log.d("error: ", error.toString()) })
+
+        requestQueue.add(stringRequest)
+
+
+
+
+
+        return authenticated
+
+    }
+
+    private fun checkUsername(username: String, requestQueue: RequestQueue): Int? {
+        var finish: Int? = null
+
+        val pwd = password.text.toString()
+        Log.d("ShowMeOnly","Password: $pwd")
+        val encodedPassword: String = Base64.getEncoder().encodeToString(password.text.toString().toByteArray())
+
+        val url = "https://dev.donsdirectory.com/api_auth.php?u=$username"
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                { response ->
+                    if (response == null) {
+                        Log.d("Houston","We have a problem")
+                    } else {
+                        Log.d("LoginActivity", "Token: $response")
+                        finish = checkPassword(response, encodedPassword, requestQueue)
+                    }
+                },
+                { error -> Log.d("error: ", error.toString()) }
+        )
+        requestQueue.add(stringRequest)
+
+        return finish
+    }
+
+
     private fun authenticateLogin(username: Editable, password: Editable): Int {
 
-        if(username.isBlank()) {
+        if (username.isBlank()) {
             makeToast("Username cannot be blank")
             return 0
         }
-        if(password.isBlank()) {
+        if (password.isBlank()) {
             makeToast("Password cannot be blank")
             return 0
         }
         makeToast("Authenticating...")
-        Log.d("button: ","clicked")
+        Log.d("button: ", "clicked")
         val requestQueue = Volley.newRequestQueue(this)
-        var feedback: String? = null
-        var url = "https://dev.donsdirectory.com/api_auth.php?u=$username"
-        var stringRequest = StringRequest(Request.Method.GET, url,
-                { response -> if(response == null) {
-                    textView.text = "Failed"
-                } else {
-                    feedback = response
-                    textView.text = "Token: $response"
-                }},
-                { error -> Log.d("error: ", error.toString())})
-        requestQueue.add(stringRequest)
-        url = "https://dev.donsdirectory.com/api_auth.php?t=$feedback&p=$password"
-        stringRequest = StringRequest(Request.Method.GET, url,
-                { response -> if(response == null) {
-//                    textView.text = "Failed"
-                } else {
-//                    textView.text = "Token: $response"
-                }},
-                { error -> Log.d("error: ", error.toString())})
-        requestQueue.add(stringRequest)
-        return 1
+
+        return checkUsername(username.toString(), requestQueue) ?: 0
     }
 }
+
+//        timeout = calendar.get(Calendar.SECOND)
+//
+//        while(token == null){
+//            if (calendar.get(Calendar.SECOND) > timeout + 1){
+//                break
+//            }
+//        }
+
+
+//    }
+//}
 //        makeToast("Authenticating...")
 //        var token: String = ""
 //        var loginResult: Int? = null
